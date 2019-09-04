3ef2ce8f66da1757dbca533139510db6919cb8be
TRAHO <traho9750@naver.com>
2019-09-04 17:36:59 +0900

    first_commit

--- ac.sau.Hello/App.tsx
+++ ac.sau.Hello/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>asssdf</Text>
+      <Text>ssdssd</Text>
     </View>
   );
 }
